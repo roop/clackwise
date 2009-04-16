8a80977b209bdbfcdfbd7e685d41a7fa1b60a17a
Roopesh Chander <roop@forwardbias.in>
2009-04-16 20:08:57 +0530

    Invoke tclreadline if found in auto_path or $env{TCLLIBPATH}

--- release/lib/clackwise/init.tcl
+++ release/lib/clackwise/init.tcl
@@ -1,3 +1,8 @@
 puts "Loading ClackwiseLiberty module ...\n"
 load ${clackwise_root_path}/lib/libClackwiseLiberty.so ClackwiseLiberty
 
+# Activate tcl readline if found
+if {![catch {package require tclreadline}]} {
+	::tclreadline::Loop
+}
+

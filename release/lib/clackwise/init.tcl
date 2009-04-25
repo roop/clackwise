a4980def54adab0abc4d472fbf12835a87a58927
Roopesh Chander <roop@forwardbias.in>
2009-04-25 22:55:30 +0530

    Temporarily s/(so|lib)/dylib for using the mac for dev

--- release/lib/clackwise/init.tcl
+++ release/lib/clackwise/init.tcl
@@ -1,5 +1,5 @@
 puts "Loading ClackwiseLiberty module ...\n"
-load ${clackwise_root_path}/lib/libClackwiseLiberty.so ClackwiseLiberty
+load ${clackwise_root_path}/lib/libClackwiseLiberty.dylib ClackwiseLiberty
 
 # Activate tcl readline if found
 if {![catch {package require tclreadline}]} {

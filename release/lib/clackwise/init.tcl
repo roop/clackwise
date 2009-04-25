94c1a9e994e5249026295769a5e16fab860ac442
Roopesh Chander <roop@forwardbias.in>
2009-04-25 23:30:51 +0530

    Add the Tcl cmdline package to the bundle

--- release/lib/clackwise/init.tcl
+++ release/lib/clackwise/init.tcl
@@ -1,3 +1,8 @@
+# Load cmdline module
+lappend auto_path ${clackwise_root_path}/lib/cmdline
+package require cmdline
+
+# Load clackwise liberty module
 puts "Loading ClackwiseLiberty module ...\n"
 load ${clackwise_root_path}/lib/libClackwiseLiberty.dylib ClackwiseLiberty
 

c36c100b56745ba21e5e2f8df1c469e11d931c64
Roopesh Chander <roop@forwardbias.in>
2009-05-18 19:50:15 +0530

    Get back to linux (revert a4980)

--- release/lib/clackwise/init.tcl
+++ release/lib/clackwise/init.tcl
@@ -4,7 +4,7 @@ package require cmdline
 
 # Load clackwise liberty module
 puts "Loading ClackwiseLiberty module ...\n"
-load ${clackwise_root_path}/lib/libClackwiseLiberty.dylib ClackwiseLiberty
+load ${clackwise_root_path}/lib/libClackwiseLiberty.so ClackwiseLiberty
 source ${clackwise_root_path}/lib/clackwise/clackwise_liberty.tcl
 
 # Activate tcl readline if found

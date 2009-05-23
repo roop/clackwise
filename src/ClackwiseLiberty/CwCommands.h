b945e8839f20d43e7387609b7be5b8771069d77e
Roopesh Chander <roop@forwardbias.in>
2009-05-23 15:49:14 +0530

    Qualify types in swigged commands with the C++ namespace (so that their pointer types in Tcl are correct)

--- src/ClackwiseLiberty/CwCommands.h
+++ src/ClackwiseLiberty/CwCommands.h
@@ -27,10 +27,10 @@ version 2.1 along with Clackwise.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace Clackwise {
 
-CwLib* cw_read_lib(const QString &filename);
-QList<CwLib*> cw_get_libs(const QString &pattern, int patternSyntax);
-void write_lib(CwLib *dotlib, const QString &filename);
-QString object_to_string(CwLib *dotlib);
+Clackwise::CwLib* cw_read_lib(const QString &filename);
+QList<Clackwise::CwLib*> cw_get_libs(const QString &pattern, int patternSyntax);
+void write_lib(Clackwise::CwLib *dotlib, const QString &filename);
+QString object_to_string(Clackwise::CwLib *dotlib);
 
 }
 #endif

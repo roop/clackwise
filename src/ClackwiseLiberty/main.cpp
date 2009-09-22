1bd92304be3dfb1fb6fe8d232a51058ae233e36c
Roopesh Chander <roop@forwardbias.in>
2009-09-22 22:46:21 +0530

    do some more in main.cpp

--- src/ClackwiseLiberty/main.cpp
+++ src/ClackwiseLiberty/main.cpp
@@ -8,6 +8,9 @@ int main(int argc, char *argv[])
 {
     CwLib* dl = cw_read_lib(QString(argv[1]));
     dl = cw_get_libs(dl->name(), QRegExp::FixedString).at(0);
+    CwLibGroup *inv1x = dl->subgroupsByName("cell", "inv1x", QRegExp::FixedString).at(0);
+    qDebug() << "name:" << inv1x->name();
+    qDebug() << "fullname:" << inv1x->fullName();
     write_lib(dl, "/tmp/out.lib");
 }
 
